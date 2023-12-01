c6a047440505f782f86e2df390d36c7b6484b304
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2023-12-01 23:09:44 +0000

    Finish Day 1

---
+++ AdventOfCode/Day1.fs
@@ -0,0 +1,59 @@
+﻿module Day1
+
+open System
+
+let removeLetters (line: string): int list =
+    line |> Seq.toList |> List.filter Char.IsNumber |> List.map (string >> int)
+
+// This is a 'parameterised active pattern' - let's you define your own pattern matching 🤯
+let (|Prefix|_|) (prefix: string) (s : string) =
+    if s.StartsWith(prefix) then
+        Some(s.Substring(prefix.Length))
+    else
+        None
+
+let rec consumeNumberWords (line: string): int list =
+    let rest = line[1..]
+    match line with
+    | Prefix "one" _ -> 1 :: consumeNumberWords rest
+    | Prefix "two" _ -> 2 :: consumeNumberWords rest
+    | Prefix "three" _ -> 3 :: consumeNumberWords rest
+    | Prefix "four" _ -> 4 :: consumeNumberWords rest
+    | Prefix "five" _ -> 5 :: consumeNumberWords rest
+    | Prefix "six" _ -> 6 :: consumeNumberWords rest
+    | Prefix "seven" _ -> 7 :: consumeNumberWords rest
+    | Prefix "eight" _ -> 8 :: consumeNumberWords rest
+    | Prefix "nine" _ -> 9 :: consumeNumberWords rest
+    | Prefix "1" _ -> 1 :: consumeNumberWords rest
+    | Prefix "2" _ -> 2 :: consumeNumberWords rest
+    | Prefix "3" _ -> 3 :: consumeNumberWords rest
+    | Prefix "4" _ -> 4 :: consumeNumberWords rest
+    | Prefix "5" _ -> 5 :: consumeNumberWords rest
+    | Prefix "6" _ -> 6 :: consumeNumberWords rest
+    | Prefix "7" _ -> 7 :: consumeNumberWords rest
+    | Prefix "8" _ -> 8 :: consumeNumberWords rest
+    | Prefix "9" _ -> 9 :: consumeNumberWords rest
+    | "" -> []
+    | _ -> consumeNumberWords rest
+
+let firstAndLastDigit (digits: int list) =
+    if List.isEmpty digits then
+        0
+    else
+        let first = List.head digits
+        let last = List.last digits
+        10 * first + last
+
+let solvePart1 (lines: string list) =
+    lines
+    |> List.map removeLetters
+    |> List.map firstAndLastDigit
+    |> List.sum
+    |> string
+
+let solvePart2 (lines: string list) =
+    lines
+    |> List.map consumeNumberWords
+    |> List.map firstAndLastDigit
+    |> List.sum
+    |> string
---
+++ AdventOfCode/Program.fsx
@@ -0,0 +1,30 @@
+﻿#load "Day1.fs"
+
+open System.IO
+
+let solvePart1 (day: int) (lines: string list) =
+    match day with
+    | 1 -> Day1.solvePart1 lines
+    | _ -> "invalid day"
+
+let solvePart2 (day: int) (lines: string list) =
+    match day with
+    | 1 -> Day1.solvePart2 lines
+    | _ -> "invalid day"
+
+let readLines (day: int) =
+    let filename day =
+        Path.Combine(__SOURCE_DIRECTORY__, $"Input/day{day}.txt")
+
+    File.ReadAllLines(filename day) |> Array.toList
+
+let printSolutions (day: int) =
+    let lines = readLines day
+    let part1Solution = solvePart1 day lines
+    let part2Solution = solvePart2 day lines
+    printfn $"Part 1: {part1Solution}"
+    printfn $"Part 2: {part2Solution}"
+
+match fsi.CommandLineArgs with
+| [| _; day |] -> printSolutions (int day)
+| _ -> eprintfn "usage: <day>"
