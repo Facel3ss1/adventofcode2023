0d9c3e661967004173307ef49e100cc0ecfbfda3
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2023-12-01 23:34:47 +0000

    Make Day 1 cleaner using active patterns

--- AdventOfCode/Day1.fs
+++ AdventOfCode/Day1.fs
@@ -2,37 +2,39 @@
 
 open System
 
-let removeLetters (line: string): int list =
-    line |> Seq.toList |> List.filter Char.IsNumber |> List.map (string >> int)
+let inline charToInt (c: char) = int c - int '0'
 
-// This is a 'parameterised active pattern' - let's you define your own pattern matching 🤯
-let (|Prefix|_|) (prefix: string) (s : string) =
+let removeLetters (line: string): int list =
+    line |> Seq.toList |> List.filter Char.IsNumber |> List.map charToInt
+
+// This is a 'active pattern' - let's you define your own pattern matching 🤯
+let (|StringPrefix|_|) (prefix: string) (s : string) =
     if s.StartsWith(prefix) then
         Some(s.Substring(prefix.Length))
+    else
+        None
+
+let (|DigitPrefix|_|) (s : string) =
+    if String.length s = 0 then
+        None
+    else if Char.IsNumber s[0] then
+        Some(charToInt s[0])
     else
         None
 
 let rec consumeNumberWords (line: string): int list =
     let rest = line[1..]
     match line with
-    | Prefix "one" _ -> 1 :: consumeNumberWords rest
-    | Prefix "two" _ -> 2 :: consumeNumberWords rest
-    | Prefix "three" _ -> 3 :: consumeNumberWords rest
-    | Prefix "four" _ -> 4 :: consumeNumberWords rest
-    | Prefix "five" _ -> 5 :: consumeNumberWords rest
-    | Prefix "six" _ -> 6 :: consumeNumberWords rest
-    | Prefix "seven" _ -> 7 :: consumeNumberWords rest
-    | Prefix "eight" _ -> 8 :: consumeNumberWords rest
-    | Prefix "nine" _ -> 9 :: consumeNumberWords rest
-    | Prefix "1" _ -> 1 :: consumeNumberWords rest
-    | Prefix "2" _ -> 2 :: consumeNumberWords rest
-    | Prefix "3" _ -> 3 :: consumeNumberWords rest
-    | Prefix "4" _ -> 4 :: consumeNumberWords rest
-    | Prefix "5" _ -> 5 :: consumeNumberWords rest
-    | Prefix "6" _ -> 6 :: consumeNumberWords rest
-    | Prefix "7" _ -> 7 :: consumeNumberWords rest
-    | Prefix "8" _ -> 8 :: consumeNumberWords rest
-    | Prefix "9" _ -> 9 :: consumeNumberWords rest
+    | StringPrefix "one" _ -> 1 :: consumeNumberWords rest
+    | StringPrefix "two" _ -> 2 :: consumeNumberWords rest
+    | StringPrefix "three" _ -> 3 :: consumeNumberWords rest
+    | StringPrefix "four" _ -> 4 :: consumeNumberWords rest
+    | StringPrefix "five" _ -> 5 :: consumeNumberWords rest
+    | StringPrefix "six" _ -> 6 :: consumeNumberWords rest
+    | StringPrefix "seven" _ -> 7 :: consumeNumberWords rest
+    | StringPrefix "eight" _ -> 8 :: consumeNumberWords rest
+    | StringPrefix "nine" _ -> 9 :: consumeNumberWords rest
+    | DigitPrefix digit -> digit :: consumeNumberWords rest
     | "" -> []
     | _ -> consumeNumberWords rest
 
