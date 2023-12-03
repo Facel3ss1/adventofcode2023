f76ff0c2ca9a071c4e8296858852d2a7d66ebf57
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2023-12-03 16:18:30 +0000

    Update Program.fs

--- AdventOfCode/Program.fs
+++ AdventOfCode/Program.fs
@@ -1,16 +1,16 @@
 ﻿open System.IO
 
-let solvePart1 (day: int) (lines: string list) =
+let part1Solver day =
     match day with
-    | 1 -> Day1.solvePart1 lines
-    | 2 -> Day2.solvePart1 lines
-    | _ -> "invalid day"
+    | 1 -> Day1.solvePart1
+    | 2 -> Day2.solvePart1
+    | _ -> fun _ -> "not solved"
 
-let solvePart2 (day: int) (lines: string list) =
+let part2Solver day =
     match day with
-    | 1 -> Day1.solvePart2 lines
-    | 2 -> Day2.solvePart2 lines
-    | _ -> "invalid day"
+    | 1 -> Day1.solvePart2
+    | 2 -> Day2.solvePart2
+    | _ -> fun _ -> "not solved"
 
 let readLines (day: int) =
     let filename day =
@@ -20,15 +20,17 @@ let readLines (day: int) =
 
 let printSolutions (day: int) =
     let lines = readLines day
-    let part1Solution = solvePart1 day lines
-    let part2Solution = solvePart2 day lines
+    let part1Solution = part1Solver day <| lines
+    let part2Solution = part2Solver day <| lines
     printfn $"Part 1: {part1Solution}"
     printfn $"Part 2: {part2Solution}"
 
 [<EntryPoint>]
 let main (args: string array) =
     match args with
-    | [| day |] -> printSolutions (int day)
-    | _ -> eprintfn "usage: <day>"
-
-    0
+    | [| day |] ->
+        printSolutions (int day)
+        0
+    | _ ->
+        eprintfn "usage: <day>"
+        1
